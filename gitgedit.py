b8a0c9fe70775baa7cceeb3c9a655834af9fc4e4
Diogo Resende <dresende@thinkdigital.pt>
2010-10-30 02:08:14 +0100

    locked interface when commiting

--- gitgedit.py
+++ gitgedit.py
@@ -1,7 +1,9 @@
 import gtk
+# perhaps I could use only 1 or 2 instead of the 3 :)
 import os
 import commands
 import subprocess
+
 import gedit
 
 toolbar_ui_str = """<ui>
@@ -16,28 +18,24 @@ toolbar_ui_str = """<ui>
 
 class GitGeditWindowHelper:
 	def __init__(self, plugin, window):
-		#print "Plugin created for", window
 		self._window = window
 		self._plugin = plugin
 		self._last_uri = None
 		self._insert_toolbar()
 
 	def deactivate(self):
-		#print "Plugin stopped for", self._window
 		self._remove_toolbar()
 		self._window = None
 		self._plugin = None
 	
 	def _insert_toolbar(self):
 		manager = self._window.get_ui_manager()
-		
-		# Create a new action group
+
 		self._action_group = gtk.ActionGroup("GitGeditPluginActions")
 		self._action_group.add_actions([("GitAdd", gtk.STOCK_ADD, _("Git Add"), None, _("Add file to staged items"), self._toolbar_git_add)])
 		self._action_group.add_actions([("GitAddActive", gtk.STOCK_ADD, _("Git Add Active"), None, _("Add active files to staged items"), self._toolbar_git_add_active)])
 		self._action_group.add_actions([("GitCommit", gtk.STOCK_SAVE, _("Git Commit"), None, _("Commit staged items"), self._toolbar_git_commit)])
 
-		# Insert the action group
 		manager.insert_action_group(self._action_group, -1)
 		
 		self._toolbar_ui = manager.add_ui_from_string(toolbar_ui_str)
@@ -94,12 +92,16 @@ class GitGeditWindowHelper:
 		
 		if len(text) == 0:
 			return
+		
+		dialog.get_widget("commit_changes").set_sensitive(False)
+		dialog.get_widget("commit_text").set_sensitive(False)
+		dialog.get_widget("commit_button").set_sensitive(False)
 
 		os.chdir(os.path.dirname(self._last_uri))
 		
 		subprocess.Popen([ "git", "commit", "-m", text ])
 		
-		dialog.get_widget("commit_window").desotry()
+		dialog.get_widget("commit_window").destroy()
 	
 	def _git_add_file(self, path):
 		if path == None:
