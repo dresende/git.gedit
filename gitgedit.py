4225c399ee84446245f7a5db2747edccc8a8d712
Diogo Resende <dresende@thinkdigital.pt>
2010-11-09 21:53:24 +0000

    last ui defs organizin (I hope..)

--- gitgedit.py
+++ gitgedit.py
@@ -28,53 +28,6 @@ class GitGeditWindowHelper:
 		self._window = None
 		self._plugin = None
 	
-	def _toolbar_git_add(self, action):
-		document = self._window.get_active_tab().get_document()
-		
-		self._git_add_file(document.get_uri())
-
-	def _toolbar_git_add_active(self, action):
-		documents = self._window.get_documents()
-		
-		for document in documents:
-			self._git_add_file(document.get_uri())
-
-	def _toolbar_git_commit(self, action):
-		document = self._window.get_active_tab().get_document()
-		
-		path = document.get_uri()
-		if path == None:
-			self._alert("No document is active")
-			return
-		
-		path = self._normalize_path(path)
-
-		dialog = gtk.glade.XML("gitgedit.commit.glade", "commit_window")
-		
-		treeview = dialog.get_widget("commit_changes")
-		
-		column = gtk.TreeViewColumn("File", gtk.CellRendererText(), text=0)
-		column.set_resizable(True)
-		column.set_sort_column_id(0)
-		treeview.append_column(column)
-		
-		changes_list = gtk.ListStore(str)
-		treeview.set_model(changes_list)
-		
-		os.chdir(os.path.dirname(path))
-		output = commands.getoutput("git status --porcelain")
-		
-		for line in output.splitlines():
-			if line.startswith("?? "):
-				continue
-			changes_list.append([ line[3:] ])
-		
-		dialog.get_widget("commit_button").connect("clicked", self._git_commit, dialog, path)
-		dialog.get_widget("commit_window").show()
-	
-	def _toolbar_git_push(self, action):
-		pass
-	
 	def _git_commit(self, widget, dialog, path):
 		text = dialog.get_widget("commit_text").get_text()
 		
@@ -109,10 +62,10 @@ class GitGeditWindowHelper:
 		manager = self._window.get_ui_manager()
 
 		self._action_group = gtk.ActionGroup("GitGeditPluginActions")
-		self._action_group.add_actions([("GitAdd", gtk.STOCK_DND, _("Git Add"), None, _("Add file to staged items"), self._toolbar_git_add)])
-		self._action_group.add_actions([("GitAddActive", gtk.STOCK_DND_MULTIPLE, _("Git Add Active"), None, _("Add active files to staged items"), self._toolbar_git_add_active)])
-		self._action_group.add_actions([("GitCommit", gtk.STOCK_SAVE_AS, _("Git Commit"), None, _("Commit staged items"), self._toolbar_git_commit)])
-		self._action_group.add_actions([("GitPush", gtk.STOCK_GO_UP, _("Git Push"), None, _("Push commits from master to origin"), self._toolbar_git_push)])
+		self._action_group.add_actions([("GitAdd", gtk.STOCK_DND, _("Git Add"), None, _("Add file to staged items"), self.ui_toolbar_git_add)])
+		self._action_group.add_actions([("GitAddActive", gtk.STOCK_DND_MULTIPLE, _("Git Add Active"), None, _("Add active files to staged items"), self.ui_toolbar_git_add_active)])
+		self._action_group.add_actions([("GitCommit", gtk.STOCK_SAVE_AS, _("Git Commit"), None, _("Commit staged items"), self.ui_toolbar_git_commit)])
+		self._action_group.add_actions([("GitPush", gtk.STOCK_GO_UP, _("Git Push"), None, _("Push commits from master to origin"), self.ui_toolbar_git_push)])
 
 		manager.insert_action_group(self._action_group, -1)
 		
@@ -123,6 +76,53 @@ class GitGeditWindowHelper:
 		manager.remove_ui(self._toolbar_ui)
 		manager.remove_action_group(self._action_group)
 		manager.ensure_update()
+	
+	def ui_toolbar_git_add(self, action):
+		document = self._window.get_active_tab().get_document()
+		
+		self._git_add_file(document.get_uri())
+
+	def ui_toolbar_git_add_active(self, action):
+		documents = self._window.get_documents()
+		
+		for document in documents:
+			self._git_add_file(document.get_uri())
+
+	def ui_toolbar_git_commit(self, action):
+		document = self._window.get_active_tab().get_document()
+		
+		path = document.get_uri()
+		if path == None:
+			self._alert("No document is active")
+			return
+		
+		path = self._normalize_path(path)
+
+		dialog = gtk.glade.XML("gitgedit.commit.glade", "commit_window")
+		
+		treeview = dialog.get_widget("commit_changes")
+		
+		column = gtk.TreeViewColumn("File", gtk.CellRendererText(), text=0)
+		column.set_resizable(True)
+		column.set_sort_column_id(0)
+		treeview.append_column(column)
+		
+		changes_list = gtk.ListStore(str)
+		treeview.set_model(changes_list)
+		
+		os.chdir(os.path.dirname(path))
+		output = commands.getoutput("git status --porcelain")
+		
+		for line in output.splitlines():
+			if line.startswith("?? "):
+				continue
+			changes_list.append([ line[3:] ])
+		
+		dialog.get_widget("commit_button").connect("clicked", self._git_commit, dialog, path)
+		dialog.get_widget("commit_window").show()
+	
+	def ui_toolbar_git_push(self, action):
+		pass
 
 	def ui_update(self):
 		active_tab = self._window.get_active_tab()
